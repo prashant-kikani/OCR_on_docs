b979f8df1e32bc2430c88faa49727057d4494894
Prashant Kikani <pvkikani1997@gmail.com>
2019-03-27 11:34:20 +0530

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -13,7 +13,7 @@ Shipper, Consignee, Chargeable Weight, Product Description
 
 # Format is, key : a tuple. 1st element of tuple is bool showing whether its distributive word or not. 2nd item is, all the synonyms.
 imp = {
-	'awb': (False, ['awb', 'master', 'mawb', 'airway', 'bill']),
+	'mawb': (False, ['mawb', 'master', 'awb', 'airway', 'bill']),
 	'origin': (False, ['origin']),
 	'destination': (False, ['destination']),
 	'departure': (False, ['departure', 'loading', 'discharge']),
@@ -24,11 +24,11 @@ imp = {
 	'discription': (True, ['discription']) 
 }
 
-rotate_thresh = 2
+rotate_thresh = 2	# if less than this items found, then rotate image might be rotated. We rotate by 270 or 90 degree & try again.
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 dirr = 'all'
-n_img_to_try = 1														# Test run tesseract on these many randomly selected images from 200 images.
+no_img_to_try = 1	# Test run tesseract on these many randomly selected images from ACM 200 images.
 
 # Improvement: Do for each possible font to reduce time.
 # Preprocessing on image : like thresholding on image. To check whether it improves performance or not.
@@ -38,7 +38,7 @@ def read_img(img):
 	'''
 	img: PIL Image object
 
-	returns:
+	returns
 	text in the img.
 	'''
 	text = pytesseract.image_to_string(img, lang='eng')
@@ -61,30 +61,29 @@ def search_for(words, sents, is_dis):
 			if word in j:
 				print(j)
 				found = 1
-				# print(word, 'in:', j)
 				if first:
-					# print('relevent sentence(s) for ', word, ':')
 					first = False
 				if is_dis:
-					print(*[x for x in sents[ind + 1 : ind + 4] if word not in x], sep = '\n')
+					print(*[x for x in sents[ind + 1 : ind + 3] if word not in x], sep = '\n')
 					# print(*sents[ind : ind + 4], sep = '\n')
 				else:
-					print(*[x for x in sents[ind + 1 : ind + 2] if word not in x], sep = '\n')
+					print(*[x for x in sents[ind + 1 : ind + 3] if word not in x], sep = '\n')
 					# print(*sents[ind : ind + 2], sep = '\n')
 		if found == 1:
 			break
-
+	print()
 	return found
 
 # found all the words in the give text.
 def found_it(text, imp):
 	'''
 	text: Text read by OCR
-	imp: map of our search list.
+	imp: map of our search list words.
 
 	returns:
 	total_found: total occurance founded
-	sents: all the sentences.
+	sents: all the sentences from text.
+
 	'''
 	sents = re.split('\n', text)
 	sents = list(filter(None, sents))
@@ -97,14 +96,14 @@ def found_it(text, imp):
 	
 	total_found = 0
 	for k, v in imp.items():
-		found = search_for(words = v[1], sents = sents, is_dis = k[0])
+		found = search_for(words = v[1], sents = sents, is_dis = v[0])
 		total_found += found
 	return total_found, sents
 
 
 # Choose random images form 200 images & check results.
 all_imgs = os.listdir(dirr)
-for _ in range(n_img_to_try):
+for _ in range(no_img_to_try):
 	ra_ind = np.random.randint(0, len(all_imgs))
 	f = all_imgs[ra_ind]
 	print(str(f) + "."*50)
@@ -115,24 +114,25 @@ for _ in range(n_img_to_try):
 	img.show()
 	# Image might be rotated. So, try different angles.
 	if total_found < rotate_thresh: 
-		print('Failed to found more than 2. Rotating by 270')
+		print('Failed to found more than 2. Image might be rotated. Rotating by 270')
 		img1 = img.transpose(Image.ROTATE_270)
 		text = read_img(img = img1)
 		total_found, sents = found_it(text, imp)
 		img1.show()
 
 		if total_found < rotate_thresh:
-			print('Again failed to found more than 2. Rotating by 90')
+			print('Again failed to found more than 2. Image might be rotated. Rotating by 90')
 			img1 = img.transpose(Image.ROTATE_90)
 			text = read_img(img = img1)
 			total_found, sents = found_it(text, imp)
 			img1.show()
-			
+
 			if total_found < rotate_thresh: 
 				print('BAD QUALITY!') 
 	
 	words = [i.split() for i in sents]
 	words = list(itertools.chain.from_iterable(words))   						# flatten
+	# all the word level work do here.
 
 
 
